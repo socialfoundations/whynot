bc60bd7246a4017195059ca5bdd45317be529bd7
John Miller <johnpmiller16@gmail.com>
2019-11-25 17:37:31 -0800

    Remove causality dependency.
    - Implements propensity weighted and matching without relying on the causality library.
    - Removes verbose print out and warnings that appear during estimator execution.

--- whynot/algorithms/propensity_weighted_ols.py
+++ whynot/algorithms/propensity_weighted_ols.py
@@ -1,16 +1,14 @@
 """Average treatment effect estimation from propensity weighted least squares."""
 from time import perf_counter
 
-import pandas as pd
+import numpy as np
+import statsmodels.api as sm
 
-from causality.estimation.parametric import InverseProbabilityWeightedLS
 from whynot.framework import InferenceResult
 
 
 def estimate_treatment_effect(covariates, treatment, outcome):
     """Estimate treatment effects using propensity weighted least-squares.
-
-    Wrapper around causality.estimation.parametric.InverseProbabilityWeightedLS
 
     Parameters
     ----------
@@ -29,22 +27,35 @@ def estimate_treatment_effect(covariates, treatment, outcome):
 
     """
     start_time = perf_counter()
-    data = pd.DataFrame({"treatment": treatment, "outcome": outcome})
 
-    covdict = {}
-    for j in range(covariates.shape[1]):
-        name = str(j)
-        data[name] = covariates[:, j]
-        covdict[name] = "c"
+    # Compute propensity scores with logistic regression model.
+    features = sm.add_constant(covariates, prepend=True, has_constant="add")
+    logit = sm.Logit(treatment, features)
+    model = logit.fit(disp=0)
+    propensities = model.predict(features)
 
-    ipweighter = InverseProbabilityWeightedLS()
-    ate = ipweighter.estimate_ATE(data, "treatment", "outcome", covdict)
+    # IP-weights
+    treated = treatment == 1.0
+    untreated = treatment == 0.0
+    weights = treated / propensities + untreated / (1.0 - propensities)
+
+    treatment = treatment.reshape(-1, 1)
+    features = np.concatenate([treatment, covariates], axis=1)
+    features = sm.add_constant(features, prepend=True, has_constant="add")
+
+    model = sm.WLS(outcome, features, weights=weights)
+    results = model.fit()
     stop_time = perf_counter()
 
+    # Treatment is the second variable (after the constant offset)
+    ate = results.params[1]
+    stderr = results.bse[1]
+    conf_int = tuple(results.conf_int()[1])
+
     return InferenceResult(
-        ate=ate[1],
-        stderr=None,
-        ci=(ate[0], ate[2]),
+        ate=ate,
+        stderr=stderr,
+        ci=conf_int,
         individual_effects=None,
         elapsed_time=stop_time - start_time,
     )
