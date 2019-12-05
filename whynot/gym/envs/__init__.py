fa02059f510d08546c122d798a21f36653574149
Chloe <chloehsu@berkeley.edu>
2019-12-04 18:16:24 -0800

    Add RL environment for opioid simulator.

--- whynot/gym/envs/__init__.py
+++ whynot/gym/envs/__init__.py
@@ -3,7 +3,6 @@
 from whynot.gym.envs.registration import registry, register, make, spec
 from whynot.gym.envs.ode_env import ODEEnv
 
-print("registering")
 register(
     id="HIV-v0",
     entry_point="whynot.simulators.hiv.environment:HIVEnv",
@@ -17,4 +16,13 @@ register(
     entry_point="whynot.simulators.world3.environment:World3Env",
     max_episode_steps=400,
     reward_threshold=1e5,
+)
+
+
+register(
+    id='opioid-v0',
+    entry_point='whynot.simulators.opioid.environment:OpioidEnv',
+    # The simulator starts in 2002 and ends in 2030.
+    max_episode_steps=28,
+    reward_threshold=0,
 )
