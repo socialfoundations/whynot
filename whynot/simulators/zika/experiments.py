ee23583a60118cafa42518f58461444e42c12cab
John Miller <johnpmiller16@gmail.com>
2020-03-15 19:53:36 -0700

    Add simple RCT experiment.

--- whynot/simulators/zika/experiments.py
+++ whynot/simulators/zika/experiments.py
@@ -1,85 +1,47 @@
-"""Experiments for HIV simulator."""
+"""Experiments for Zika simulator."""
 
 from whynot.dynamics import DynamicsExperiment
 from whynot.framework import parameter
-from whynot.simulators import hiv
+from whynot.simulators import zika
 
 
-__all__ = ["get_experiments", "HIVRCT", "HIVConfounding"]
+__all__ = ["get_experiments", "ZikaRCT"]
 
 
 def get_experiments():
-    """Return all experiments for HIV."""
-    return [HIVRCT, HIVConfounding]
+    """Return all experiments for zika."""
+    return [ZikaRCT]
 
 
 def sample_initial_states(rng):
     """Sample initial state by randomly perturbing the default state."""
-    state = hiv.State()
-    state.uninfected_T1 *= rng.uniform(low=0.95, high=1.05)
-    state.infected_T1 *= rng.uniform(low=0.95, high=1.05)
-    state.uninfected_T2 *= rng.uniform(low=0.95, high=1.05)
-    state.infected_T2 *= rng.uniform(low=0.95, high=1.05)
-    state.free_virus *= rng.uniform(low=0.95, high=1.05)
-    state.immune_response *= rng.uniform(low=0.95, high=1.05)
-    return state
+    state = zika.State().values()
+    state *= rng.uniform(low=0.95, high=1.05, size=state.shape)
+    return zika.State(*state)
 
+def mixed_treatment_intervention():
+    """Apply mixed strategy on all controls starting at time 0."""
+    return zika.Intervention(
+        time=0,
+        treated_bednet_use=0.5,
+        condom_use=0.5,
+        treatment_of_infected=0.5,
+        indoor_spray_use=0.5,
+    )
 
 ##################
 # RCT Experiment
 ##################
 # pylint: disable-msg=invalid-name
-#: Experiment on effect of increased drug efficacy on infected macrophages (cells/ml)
-HIVRCT = DynamicsExperiment(
+#: Experiment on effect of mixed treatment policy on infections in 20 days
+ZikaRCT = DynamicsExperiment(
     name="hiv_rct",
-    description="Study effect of increased drug efficacy on infected macrophages (cells/ml).",
-    simulator=hiv,
-    simulator_config=hiv.Config(epsilon_1=0.1, start_time=0, end_time=150),
-    intervention=hiv.Intervention(time=100, epsilon_1=0.5),
+    description="Study effect of mixed treatment policy on infections in 20 days.",
+    simulator=zika,
+    simulator_config=zika.Config(start_time=0, end_time=20),
+    intervention=mixed_treatment_intervention(),
     state_sampler=sample_initial_states,
     propensity_scorer=0.5,
-    outcome_extractor=lambda run: run[149].infected_T2,
+    outcome_extractor=lambda run: run[19].symptomatic_humans,
     covariate_builder=lambda run: run.initial_state.values(),
-)
-
-
-##########################
-# Confounding Experiments
-##########################
-
-
-@parameter(
-    name="treatment_bias",
-    default=0.9,
-    description="Treatment probability bias between more infected and less infected units.",
-)
-def hiv_confounded_propensity(untreated_run, treatment_bias):
-    """Probability of treating each unit.
-
-    We are more likely to treat units with high immune response and free virus.
-    """
-    if (
-        untreated_run.initial_state.immune_response > 10
-        and untreated_run.initial_state.free_virus > 1
-    ):
-        return treatment_bias
-
-    return 1.0 - treatment_bias
-
-
-# pylint: disable-msg=invalid-name
-#: Experiment on effect of increased drug efficacy on infected macrophages with confounding
-HIVConfounding = DynamicsExperiment(
-    name="hiv_confounding",
-    description=(
-        "Study effect of increased drug efficacy on infected macrophages (cells/ml). "
-        "Units with high immune response and free virus are more likely to be treated."
-    ),
-    simulator=hiv,
-    simulator_config=hiv.Config(epsilon_1=0.1, start_time=0, end_time=150),
-    intervention=hiv.Intervention(time=100, epsilon_1=0.5),
-    state_sampler=sample_initial_states,
-    propensity_scorer=hiv_confounded_propensity,
-    outcome_extractor=lambda run: run[149].infected_T2,
-    covariate_builder=lambda intervention, run: run.initial_state.values(),
 )
